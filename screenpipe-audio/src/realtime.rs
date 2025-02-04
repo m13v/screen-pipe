c4ee2e83a4ce6f43cce260a1cd832262db0731f2
matthew-heartful <matthew.heartful@gmail.com>
2025-02-03 17:00:53 -0800

    save my audio changes before merge

--- screenpipe-audio/src/realtime.rs
+++ screenpipe-audio/src/realtime.rs
@@ -2,12 +2,13 @@ use crate::{deepgram::stream_transcription_deepgram, AudioStream};
 use anyhow::Result;
 use chrono::{DateTime, Utc};
 use screenpipe_core::Language;
-use serde::Serialize;
+use serde::{Deserialize, Serialize};
 use std::sync::{atomic::AtomicBool, Arc};
 use tokio::sync::broadcast;
 
 pub async fn realtime_stt(
     stream: Arc<AudioStream>,
+<<<<<<< HEAD
     languages: Vec<Language>,
     realtime_transcription_sender: Arc<broadcast::Sender<RealtimeTranscriptionEvent>>,
     is_running: Arc<AtomicBool>,
@@ -20,11 +21,23 @@ pub async fn realtime_stt(
         is_running,
         deepgram_api_key,
     ).await?;
+=======
+    languages: Arc<Vec<Language>>,
+    is_running: Arc<AtomicBool>,
+    deepgram_api_key: Option<String>,
+) -> Result<()> {
+    stream_transcription_deepgram(stream, languages, is_running, deepgram_api_key).await?;
+>>>>>>> upstream/main
 
     Ok(())
 }
 
+<<<<<<< HEAD
 #[derive(Debug, Clone, Serialize)]
+=======
+#[derive(Serialize, Deserialize, Clone)]
+#[serde(rename_all = "camelCase")]
+>>>>>>> upstream/main
 pub struct RealtimeTranscriptionEvent {
     pub timestamp: DateTime<Utc>,
     pub device: String,
